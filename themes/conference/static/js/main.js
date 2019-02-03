783f83fbcb28ec4dcb6b7372acb4864b1f86f935
Daniel Mahlow <dmahlow@gmail.com>
2019-02-03 01:43:42 +0100

    Add theme.

---
+++ themes/conference/static/js/main.js
@@ -0,0 +1,42 @@
+(function () {
+    'use strict';
+
+    var conf = {};
+
+    // Init functions, called on DOMContentLoaded event
+    conf.init = function () {
+        conf.menu.init();
+    };
+
+    /***
+        Create animated scroll for menu links
+    ***/
+    conf.menu = {
+        itemsSelector: '.nav-link[href^="#"]',
+        animationSpeed: 400
+    };
+
+    conf.menu.init = function () {
+        conf.menu.menuItems = $(conf.menu.itemsSelector);
+        conf.menu.document = $('html, body');
+
+        conf.menu.menuItems.on('click.animateScroll', function (event) {
+            event.preventDefault();
+
+            conf.menu.animateTo(event.target);
+        });
+    };
+
+    conf.menu.animateTo = function (link) {
+
+        var $link = $(link),
+            href = $link.attr('href'),
+            offSetTop = $(href).offset().top;
+
+        conf.menu.document.finish().animate({scrollTop : offSetTop}, conf.menu.animationSpeed, function () {
+            location.hash = href;
+        });
+    };
+
+    conf.init();
+}());
